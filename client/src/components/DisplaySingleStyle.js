0b07ad2ad2466c647eb9b772532aa6e113f36efc
Zachary Sain <zachsain@gmail.com>
2023-01-13 16:35:02 -0600

    single style page

--- client/src/components/DisplaySingleStyle.js
+++ client/src/components/DisplaySingleStyle.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {useParams} from 'react-router-dom';
+
 
 function DisplaySingleStyle() {
+    const params = useParams();
 
   return (
     <div>DisplaySingleStyle</div>
   )
-  
+
 }
 
 
--- client/src/components/StylesCard.js
+++ client/src/components/StylesCard.js
@@ -5,16 +5,18 @@ import '../App.css';
 function StylesCard({name, photo, description, id}) {
    const history = useHistory()
    
-    function handleClick(e){
+    function handleStyleClick(e){
         history.push(`/styles/${id}`)
     }
 
   return (
-    <div onClick={handleClick} className="style-card-container">
-        <div className="style-card">
-        <h3 className="style-name">{name}</h3>
-        <img className="style-img" src={photo} alt="brand-image" />
-        <p className="brand-description">{description}</p>
+    <div onClick={handleStyleClick} className="style-card-container">
+        <div > 
+            <div className="style-card">
+            <h3 className="style-name">{name}</h3>
+            <img className="style-img" src={photo} alt="brand-image" />
+            <p className="brand-description">{description}</p>
+        </div>
     </div>
 </div>
   )
